0e935257ca8d9ca60872fd829c21fa627ee467ae
Lars Ortlepp <job@larsactionhero.com>
2024-06-05 14:41:36 +0200

    refactor to class

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const bsbDetectBreakpointSizes = [
-  '--bs-breakpoint-xxl',
-  '--bs-breakpoint-xl',
-  '--bs-breakpoint-lg',
-  '--bs-breakpoint-md',
-  '--bs-breakpoint-sm',
-  '--bs-breakpoint-xs'
-];
 
-const bsbDetectGetBSBreakPoint = () => {
-  let matches = null;
-  bsBreakpointSizes.forEach((size) => {
-    const doc = document.documentElement;
-    const value = window.getComputedStyle(doc).getPropertyValue(`--breakpoint-${size}`);
-    matches = window.matchMedia(`(min-width: ${value})`).matches;
-  });
+export default class bsBreakpointDetect {
+  constructor() {
+    this.bsbDetectBreakpointSizes = [
+      '--bs-breakpoint-xxl',
+      '--bs-breakpoint-xl',
+      '--bs-breakpoint-lg',
+      '--bs-breakpoint-md',
+      '--bs-breakpoint-sm',
+      '--bs-breakpoint-xs'
+    ];
+  }
 
-  window.currentBreakpoint = matches;
-  document.body.dataset.currentBpBreakpoint = matches;
-  return matches;
+  bsbDetectGetBSBreakPoint() {
+    let matches = null;
+    this.bsBreakpointSizes.forEach((size) => {
+      const doc = document.documentElement;
+      const value = window.getComputedStyle(doc).getPropertyValue(`--breakpoint-${size}`);
+      matches = window.matchMedia(`(min-width: ${value})`).matches;
+    });
+  
+    window.currentBreakpoint = matches;
+    document.body.dataset.currentBpBreakpoint = matches;
+    return matches;
+  }
+  
+  init() {
+    document.addEventListener('DOMContentLoaded', bsbDetectGetBSBreakPoint);
+  
+    const bsbDetectResizeTimer = null;
+    document.addEventListener('resize', () => {
+      if (bsbDetectResizeTimer) clearTimeout;
+      bsbDetectResizeTimer = setTimeout(() => bsbDetectGetBSBreakPoint, 100);
+    });
+  }
 }
-
-document.addEventListener('DOMContentLoaded', bsbDetectGetBSBreakPoint);
-
-const bsbDetectResizeTimer = null;
-document.addEventListener('resize', () => {
-  if (bsbDetectResizeTimer) clearTimeout;
-  bsbDetectResizeTimer = setTimeout(() => bsbDetectGetBSBreakPoint, 100);
-});
